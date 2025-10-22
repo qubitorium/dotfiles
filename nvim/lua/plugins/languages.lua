a573f57af50386c6a6203f4a6d250d6295c5dbf1
qubitorium <qubit.32@proton.me>
2025-10-22 03:44:03 +0100

    Added emmet-language-server lsp support

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -2,7 +2,11 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-            require("lspconfig").basedpyright.setup({
+            local lspconfig = require("lspconfig")
+            local util = require("lspconfig.util")
+
+            -- Python
+            lspconfig.basedpyright.setup({
                 settings = {
                     python = {
                         analysis = {
@@ -15,10 +19,49 @@ return {
                 },
             })
 
-            require("lspconfig").clangd.setup({
+            -- C / C++
+            lspconfig.clangd.setup({
                 cmd = { "clangd", "--background-index" },
                 filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
                 single_file_support = true,
+            })
+
+            -- Emmet
+            lspconfig.emmet_language_server.setup({
+                cmd = { "emmet-language-server", "--stdio" },
+                filetypes = {
+                    "astro",
+                    "css",
+                    "eruby",
+                    "html",
+                    "htmlangular",
+                    "htmldjango",
+                    "javascriptreact",
+                    "less",
+                    "pug",
+                    "sass",
+                    "scss",
+                    "svelte",
+                    "templ",
+                    "typescriptreact",
+                    "vue",
+                },
+                root_dir = function(fname)
+                    -- starts with searching for git or file directory or cwd
+                    return util.find_git_ancestor(fname) or util.path.dirname(fname) or vim.loop.cwd()
+                end,
+                single_file_support = true,
+                init_options = {
+                    includeLanguages = {},
+                    excludeLanguages = {},
+                    extensionsPath = {},
+                    preferences = {},
+                    showAbbreviationSuggestions = true,
+                    showExpandedAbbreviation = "always",
+                    showSuggestionsAsSnippets = false,
+                    syntaxProfiles = {},
+                    variables = {},
+                },
             })
         end,
     },
