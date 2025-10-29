3da97af73a1f6283f15f6617dbca701081ff93ea
qubitorium <qubit.32@proton.me>
2025-10-29 02:27:28 +0100

    added some useful keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- to move lines (like drag them)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- appends the line below at same line while cursor still at start of the line
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- just keeps cursor at center when u page up or down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
